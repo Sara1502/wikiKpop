806257023ef1f4b95de846293e9c71f445a9cf60
Sara1502 <moreirasaragds@gmail.com>
2024-09-06 09:49:06 -0300

    add pesquisa

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 function pesquisar() {
     let section = document.getElementById("resultados-pesquisa");
 
-    let resultados = "";
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    for (let dado of dados) {
-        resultados += `
-            <div class="item-resultado">
-                <h2>${dado.titulo}</h2>
-                <p class="descricao-meta">${dado.descricao}</p>
-                <a href=${dado.insta}, target="_blank">
-                    Instagram
-                </a>
-            </div>  
-        `
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Não foi encontrado</p>"
+        return
     }
 
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+            resultados += `
+                <div class="item-resultado">
+                    <h2>${dado.titulo}</h2>
+                    <p class="descricao-meta">${dado.descricao}</p>
+                    <a href=${dado.insta}, target="_blank">
+                        Instagram
+                    </a>
+                </div>  
+           `
+        }
+
+        if (!resultados){
+            resultados = "<p>Não foi encontrado</p>"
+        }
+    }
     section.innerHTML = resultados
 }
 
